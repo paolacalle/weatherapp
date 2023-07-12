4f2dc96f07b8b75b348dd8bc64beff0d4d7ef201
Joshua Bello <joshuabello2550@gmail.com>
2023-07-12 23:12:25 +0000

    registration and login

--- main.py
+++ main.py
@@ -2,44 +2,68 @@ import git
 from flask import Flask, render_template, url_for, flash, redirect, request
 from forms import RegistrationForm, loginForm
 from flask_behind_proxy import FlaskBehindProxy
-from flask_sqlalchemy import SQLAlchemy
+import sqlite3
+
+connection = sqlite3.connect('database.db')
+cursor = connection.cursor()
+
+# Create a Table
+create_table_query = '''
+    CREATE TABLE IF NOT EXISTS users (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        username TEXT,
+        email TEXT,
+        password TEXT
+    )
+'''
+cursor.execute(create_table_query)
+connection.close()
 
 app = Flask(__name__)
 proxied = FlaskBehindProxy(app)  #to handle redirect in codio 
 
 app.config['SECRET_KEY'] = "8ce4fcaac337fceeca98f8d9dddfd559"
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
-db = SQLAlchemy(app)
-
-class User(db.Model):
-  id = db.Column(db.Integer, primary_key=True)
-  username = db.Column(db.String(20), unique=True, nullable=False)
-  email = db.Column(db.String(120), unique=True, nullable=False)
-  password = db.Column(db.String(60), nullable=False)
-
-  def __repr__(self):
-    return f"User('{self.username}', '{self.email}')"
-
-with app.app_context():
-  db.create_all()
-
 @app.route("/")
 def home():
     #we call render_template instead of returning raw HTML -- this is where we point 
     #corresponding template .html file and 
     #pass the missing info 
-    
     return render_template('home.html', subtitle='Welcome to Weather App.', text='This is the home page')
 
 @app.route("/register", methods=['GET', 'POST'])
 def register():
     form = RegistrationForm()
     if form.validate_on_submit(): # checks if entries are valid
-        # user = User(username=form.username.data, email=form.email.data, password=form.password.data)
-        # db.session.add(user)
-        # db.session.commit()
-        # print(user)
+        # Establish SQLite connection and cursor
+        connection = sqlite3.connect('database.db')
+        cursor = connection.cursor()
+
+        # Check if username or email already exists in the database
+        select_query = '''
+            SELECT * FROM users WHERE username=? OR email=?
+        '''
+        cursor.execute(select_query, (form.username.data, form.email.data))
+        existing_user = cursor.fetchone()
+
+        if existing_user:
+            flash('Username or email already exists. Please choose a different username or email.', 'danger')
+            connection.close()
+            return redirect(url_for('register'))
+
+        # Insert form data into "users" table
+        insert_query = '''
+            INSERT INTO users (username, email, password)
+            VALUES (?, ?, ?)
+        '''
+        username = form.username.data
+        email = form.email.data
+        password = form.password.data
+        cursor.execute(insert_query, (username, email, password))
+        connection.commit()
+        # Close the connection
+        connection.close()
+
         flash(f'Account created for {form.username.data}!', 'success')
         return redirect(url_for('home')) # if so - send to home page
     return render_template('register.html', title='Register', form=form)
@@ -48,12 +72,25 @@ def register():
 def login():
     form = loginForm()
     if form.validate_on_submit(): # checks if entries are valid
-        # user = User(username=form.username.data, email=form.email.data, password=form.password.data)
-        # db.session.add(user)
-        # db.session.commit()
-        # print(user)
-        flash(f'Welcome back {form.username.data}!', 'success')
-        return redirect(url_for('home')) # if so - send to home page
+        connection = sqlite3.connect('database.db')
+        cursor = connection.cursor()
+
+        # Retrieve user from the database based on the provided username
+        select_query = '''
+            SELECT * FROM users WHERE username=?
+        '''
+        cursor.execute(select_query, (form.username.data,))
+        user = cursor.fetchone()
+
+        # Check if a user with the provided username exists and if the password matches
+        if user and user[3] == form.password.data:  # Assuming password is stored in the fourth column (index 3)
+            flash('Login successful!', 'success')
+            connection.close()
+            return redirect(url_for('home'))
+
+        flash('Invalid username or password. Please try again.', 'danger')
+        connection.close()
+        return redirect(url_for('login'))
     return render_template('login.html', title='Login', form=form)
 
 if __name__ == '__main__':
